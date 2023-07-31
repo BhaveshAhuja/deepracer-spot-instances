de4bcaebe12794dcbca2c90c784e5e6727713694
Bhavesh Ahuja <2016.bhavesh.ahuja@ves.ac.in>
2023-07-31 20:49:19 +0530

    direction series

--- model-2/custom-files/reward_function.py
+++ model-2/custom-files/reward_function.py
@@ -1,15 +1,16 @@
 import math
 def direction_reward(reward, waypoints, closest_waypoints, heading):
     total_length = len(waypoints)
-    prev_index = closest_waypoints[1]
+    prev_index = closest_waypoints[0]
     next_index = closest_waypoints[1]
     print("Actual Prev:- " + str(prev_index))
     print("Actual Next:- " + str(next_index))
     
-    if(next_index+6<total_length):
-        next_index = next_index+6
+    if(next_index+5<total_length):
+        next_index = next_index+5
     else:
         next_index = total_length-1
+    
     print("Modified Prev:- " + str(prev_index))
     print("Modified Next:- " + str(next_index))
     next_point = waypoints[next_index]
@@ -19,62 +20,45 @@ def direction_reward(reward, waypoints, closest_waypoints, heading):
     print("Next waypoint:- " + str(next_point))
 
     direction = math.degrees(math.atan2(next_point[1] - prev_point[1], next_point[0] - prev_point[0]))
-    direction_diff = heading - direction
+    direction_diff = direction - heading
 
     print("Calculated angle:- " + str(direction))
     print("Heading angle:- " + str(heading))
     print("Direction Diff:- " + str(direction_diff))
 
-    if direction_diff >= 20 or direction_diff < -5:
+    if direction_diff < -7:
         reward *= 0.5
-    elif direction_diff >= -5 and direction_diff < -1:
+    elif direction_diff => -7 and direction_diff < -3:
+        reward *= 0.8
+    elif direction_diff => -3 and direction_diff < -1:
         reward *= 1.2
-    elif direction_diff >= -1 and direction_diff < 6:
-        reward *= 1.6
-    elif direction_diff >= 6 and direction_diff < 10:
-        reward *= 1.3
-    elif direction_diff >=10 and direction_diff < 20:
-        reward *= 0.7
+    elif direction_diff => -1 and direction_diff <= 2:
+        reward *= 1.5
+    elif direction_diff > 2 and direction_diff <= 6:
+        reward *= 1.2
+    elif direction_diff > 6 and direction_diff <= 10:
+        reward *= 0.8
+    elif direction_diff > 10:
+        reward *= 0.5
+    
 
     return float(reward)
 
-def right_lane(reward, is_left_lane):
-    print("Is Left Lane:- " + str(is_left_lane))
-    if is_left_lane:
-        return reward*0.8
-    else:
-        return reward*1.2
-
 def reward_function(params):
     closest_waypoints = params['closest_waypoints']
-    speed = params["speed"]
-    steering_angle = params["steering_angle"]
     waypoints = params['waypoints']
     heading = params['heading']
-    is_left_of_center = params['is_left_of_center']
     is_offtrack = params["is_offtrack"]
     progress = params["progress"]
 
+    print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
     print("Current Progress:- " + str(progress))
-    print("Closest Waypoints:- " + str(closest_waypoints[0]) + ":" + str(closest_waypoints[0]))
+    print("Closest Waypoints:- " + str(closest_waypoints[0]) + ":" + str(closest_waypoints[1]))
     
     if is_offtrack:
         reward = 0.0001
         return reward
-    else:
-        if speed>2.0 and speed<2.6 and abs(steering_angle) > 20:
-            reward = 2.0
-        elif speed>2.6 and speed<3.0 and abs(steering_angle) > 0:
-            reward = 2.0
-        elif speed>3.7 and speed<3.9 and steering_angle == 0:
-            reward = 1.7
-        elif speed>3.9 and speed<4.1 and steering_angle == 0:
-            reward = 2.0
-        else:
-            reward = 0.0001
-        print("Speed Reward:- " + str(reward))
-        reward = right_lane(reward, is_left_of_center)
-        print("Right Lane Reward:- " + str(reward))
-        reward = direction_reward(reward,waypoints,closest_waypoints,heading)
+    reward = 10.0
+    reward = direction_reward(reward,waypoints,closest_waypoints,heading)
     print("Final Reward:- " + str(reward))
     return float(reward)
