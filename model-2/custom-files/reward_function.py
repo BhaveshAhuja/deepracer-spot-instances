044098c6250a1779be77840d66bf710504af4fd8
Bhavesh Ahuja <42087157+BhaveshAhuja@users.noreply.github.com>
2023-08-02 09:58:04 +0530

    Update reward_function.py

--- model-2/custom-files/reward_function.py
+++ model-2/custom-files/reward_function.py
@@ -1,6 +1,6 @@
 def reward_function(params):
 
-    if params["is_offtrack"] and params["steps"] > 0:
+    if not params["is_offtrack"] and params["steps"] > 0:
         reward = ((params["progress"] / params["steps"]) * 100) + (params["speed"]**2)
     else:
         reward = 0.01
